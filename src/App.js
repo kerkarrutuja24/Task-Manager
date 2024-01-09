bbf8face968da468e351a23bfcdbc2afbce17238
Rutuja Kerkar <73350214+kerkarrutuja24@users.noreply.github.com>
2024-01-09 20:43:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CustomButton from './components/CustomButton';
+import CustomInput from './components/CustomInput';
+import Header from './components/Header';
+import Card from './components/Card';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [add, setAdd]=useState(false);
+const [Tasks, setTasks]=useState([]);
+
+const [singleTask, setSingleTask]=useState('');
+const [singleDes, setSingleDes]=useState('');
+
+const UpdateTask =(id)=>
+{
+    setTasks(
+        Tasks.map((t)=>(
+            t.id===id? {...t,complete:true}:t
+        ))
+    )
+
+}
+const deleteTask =(id)=>
+{
+    setTasks(
+        Tasks.filter((t)=>(
+            t.id===id?false:true
+        ))
+    )
+
 }
 
-export default App;
+
+const addToCard =()=>
+{
+    const id= Tasks.length===0?1:Tasks.length+1;
+    const taskDetail=
+    {
+        id:id,
+        task:singleTask,
+        des:singleDes,
+        complete:false
+
+    }
+    setTasks(
+        [...Tasks,taskDetail]
+    )
+
+}
+
+const ClearInput =()=>
+{
+    setSingleTask('');
+    setSingleDes('');
+    
+}
+
+
+const handleCustomTask =(event)=>
+{
+    setSingleTask(event.target.value);
+}
+
+const handleCustomDes =(event)=>
+{
+    setSingleDes(event.target.value);
+}
+
+
+
+const handleInput =()=>
+{
+    setAdd(!add)
+}
+
+
+    return ( 
+        <div className='main'>
+            <div className='inputSection'>
+               <Header handleInput={handleInput}  />
+                {add===true ?
+                    <>
+                    <CustomInput  value={singleTask}  placeHolder='Enter Task' name='Task' change={handleCustomTask}  /> 
+                    <CustomInput   value={singleDes} placeHolder='Enter Description' name='Description' change={handleCustomDes}  />
+                    <div className='btnwrapper'>
+                    <CustomButton color='White' bg='#1877F2'  name='Save Task' click={addToCard} />
+                    <CustomButton color='White' bg='red'  name='Cancle'   click={ClearInput} />
+                </div>
+                    </>:null
+                }
+               
+               
+            </div>
+
+            <div className='cardSection'>
+               {
+                Tasks.map((t)=>
+                ( 
+                    <Card title={t.task} des={t.des}   key={t.id} delete={()=>deleteTask(t.id)}  
+                       update={()=>UpdateTask(t.id)}  complete={t.complete}  />
+                ))
+               }
+               {
+                }
+            </div>
+        </div>
+     );
+}
+
+export default  App;
